a5ad103266a8103d6abc7b9c819fe26bcfd45a1d
shaka <10120571+shaka123@user.noreply.gitee.com>
2023-05-20 13:40:01 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h2>home</h2>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.home {
+  width: 100px;
+  h2 {
+    color: aqua;
+  }
+}
+</style>
